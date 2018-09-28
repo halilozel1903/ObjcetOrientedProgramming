1da9e23878b8163808b36ac045df1a31187f5508
Halil Özel <halilozel1903@gmail.com>
2018-09-28 21:40:03 +0300

    Proje olusturuluyor.

---
+++ src/com/halil/ozel/Main.java
@@ -0,0 +1,15 @@
+package com.halil.ozel;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        Araba sahin = new Araba();
+        sahin.setYil(2017);
+
+        System.out.println("Şahinin yılı : "+sahin.getYil());
+
+
+    }
+}
